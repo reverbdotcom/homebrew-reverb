089061c193f529a7ad2519ff594cf20f57858e28
Erik Benoist <ebenoist@gmail.com>
2018-12-03 10:26:00 -0600

    Fix rv tap

--- rv.rb
+++ rv.rb
@@ -4,7 +4,7 @@ class Rv < Formula
   desc "AWS node resolver"
   homepage "https://github.com/reverbdotcom/rv"
   url "https://github.com/reverbdotcom/rv/releases/download/#{@version}/binaries.tar.gz"
-  sha256 File.read(File.expand_path("../SHA", __FILE__)).chomp
+  sha256 File.read(File.expand_path("../RV_SHA", __FILE__)).chomp
   version @version
 
   def install
